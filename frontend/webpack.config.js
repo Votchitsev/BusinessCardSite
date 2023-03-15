91dcfd0ee0ba8c37f75bf7a55e17b665d3020370
Dmitry <dmitry.votchitsev@gmail.com>
2023-03-15 17:46:29 +0200

    Setup eslint

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  resolve: {
-    extensions: ['.jsx', '.js'],
-  },
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'static/frontend/'),
-    filename: 'main.js',
-  },
-  watch: true,
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
-    ]
-  }
-}
+	resolve: {
+		extensions: ['.jsx', '.js'],
+	},
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'static/frontend/'),
+		filename: 'main.js',
+	},
+	watch: true,
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+};
