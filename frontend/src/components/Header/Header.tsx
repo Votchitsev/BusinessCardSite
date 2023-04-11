3e554bacbb504b1a6288e5bc3235509847f55dc6
Dmitry <dmitry.votchitsev@gmail.com>
2023-04-11 15:58:38 +0200

    drop down menu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import {useSelector} from 'react-redux';
+import {useEffect, useState} from 'react';
 import {type RootState} from '../../GlobalState/store';
 import {type Content} from '../../GlobalState/types';
+import useScrollPosition from '../../hooks/useScrollPosition';
 import './Header.css';
 
 export default function Header() {
@@ -8,28 +10,58 @@ export default function Header() {
 		state => state.language.content,
 	) as Content;
 
+	const [fixHeader, setFixHeader] = useState(false);
+	const [activeItem, setActiveItem] = useState(0);
+	const [activeDropDownMenu, setActiveDropDownMenu] = useState(false);
+
+	const position = useScrollPosition();
+
+	useEffect(() => {
+		if (position > 220) {
+			setFixHeader(true);
+			return;
+		}
+
+		setFixHeader('');
+	}, [position]);
+
+	const onClickHandler = (index: number) => {
+		setActiveItem(index);
+	};
+
+	const toggleOnClickHandler = () => {
+		setActiveDropDownMenu(!activeDropDownMenu);
+	};
+
 	return (
-		<section className='header'>
+		<section className={`header ${fixHeader ? 'header-fixed' : ''}`}>
 			<div className='header-container'>
 				<div className='header-logo'>D.Votchitsev</div>
-				<a className='header-menu-toggle'></a>
-				<ul className='header-menu-container header-menu-container--hidden'>
+				<a
+					className={`header-menu-toggle ${fixHeader ? 'toggle-black' : ''}`}
+					onClick={toggleOnClickHandler}
+				></a>
+				<ul className={
+					`header-menu-container
+					${activeDropDownMenu ? '' : 'header-menu-container--hidden'}`
+				}>
 					{
 						content.header.map(
 							(item, index) =>
 								<li
 									key={index}
-									className='header-menu-item header-menu-item--about'
+									className={
+										`header-menu-item header-menu-item
+										${index === activeItem ? 'header-menu-item--active' : ''}`
+									}
+									onClick={() => {
+										onClickHandler(index);
+									}}
 								>
 									{item}
 								</li>,
 						)
 					}
-					{/* <li className='header-menu-item header-menu-item--home header-menu-item--active'>{}</li>
-					<li className='header-menu-item header-menu-item--about'>About</li>
-					<li className='header-menu-item header-menu-item--education'>Education</li>
-					<li className='header-menu-item header-menu-item--pet-projects'>Pet-projects</li>
-					<li className='header-menu-item header-menu-item--contact'>Contact</li> */}
 				</ul>
 			</div>
 		</section>
